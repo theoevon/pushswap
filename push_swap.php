6468ff059e45aac1cf0c3af209409539c46feff3
EgloZ938 <theo.evon@epitech.eu>
2022-09-19 15:12:31 +0200

    pushswap final

--- push_swap.php
+++ push_swap.php
@@ -5,6 +5,7 @@ class pushswap{
     function __construct($la){
         $this->la = $la;
         $this->lb = [];
+        $this->string = "";
     }
 
     public function sa(){
@@ -13,9 +14,11 @@ class pushswap{
             $varEchange = $this->la[0];
             $this->la[0] = $this->la[1];
             $this->la[1] = $varEchange;
+            $this->string .= "sa ";
+            $this->algo();
         }
     }
-
+/* 
     public function sb(){
         $compteur = count($this->lb);
         if($compteur >= 2){
@@ -29,13 +32,16 @@ class pushswap{
         $this->sa();
         $this->sb();
     }
-
+ */
     public function pa(){
         $compteur = count($this->lb);
         if($compteur >= 1){
             $this->la = array_reverse($this->la);
             $this->la[] = $this->lb[0];
             $this->la = array_reverse($this->la);
+            array_shift($this->lb);
+            $this->string .= "pa ";
+            $this->algo();
         }
     }
 
@@ -45,9 +51,12 @@ class pushswap{
             $this->lb = array_reverse($this->lb);
             $this->lb[] = $this->la[0];
             $this->lb = array_reverse($this->lb);
+            array_shift($this->la);
+            $this->string .= "pb ";
+            $this->algo();
         }
     }
-
+/* 
     public function ra(){
         $first_elem = array_shift($this->la);
         $this->la[] = $first_elem;
@@ -81,6 +90,40 @@ class pushswap{
         $this->rra();
         $this->rrb();
     }
+ */
+    public function algo(){
+        $check = $this->check();
+        if(isset($this->la[0]) && isset($this->la[1])){
+            if($check == true){
+                $this->pa();
+            }
+            else{
+                if($this->la[0] < $this->la[1]){
+                    $this->pb();
+                }
+                else{
+                    $this->sa();
+                }
+            }
+        }
+    }
+
+    public function check(){
+        $sort = $this->la;
+        sort($sort);
+        if($sort == $this->la){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function __destruct(){
+        echo substr($this->string, 0, -1);
+        echo PHP_EOL;
+        //var_dump($this->la);
+    }
 
 }
 
@@ -88,4 +131,4 @@ array_shift($argv);
 $la = $argv;
 
 $exec = new pushswap($la);
-$exec->rra();
+$exec->algo();
